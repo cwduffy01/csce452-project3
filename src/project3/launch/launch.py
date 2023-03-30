a671fa1035abee6ce60e513b5c35ebeab856e5cf
msappington11 <msappington11@gmail.com>
2023-03-29 23:43:11 -0500

    g

--- src/project3/launch/launch.py
+++ src/project3/launch/launch.py
@@ -6,9 +6,9 @@ from launch.event_handlers import *
 from launch.events import *
 
 def generate_launch_description():
-    node = Node(package='turtlesim',
-                executable='turtlesim_node')
     ep = ExecuteProcess(cmd=['ros2', 'bag', 'play', 'bags/example1'])
+    node = Node(package='project3',
+                executable='track')
     event_handler = OnProcessExit(target_action=ep,
                               on_exit=[EmitEvent(event=Shutdown())])
     terminate_at_end = RegisterEventHandler(event_handler)
