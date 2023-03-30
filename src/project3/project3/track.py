51f189a5571fb331c695180bf988c9a9d5c6765c
msappington11 <msappington11@gmail.com>
2023-03-29 23:25:25 -0500

    remove possible people points from background

--- src/project3/project3/track.py
+++ src/project3/project3/track.py
@@ -2,8 +2,8 @@ import rclpy
 from rclpy.node import Node
 
 # rosdep install -i --from-path src --ignore-src -r -y --rosdistro humble
-# colcon build --packages-select project3
 # . install/setup.bash
+# colcon build --packages-select project3
 # ros2 run project3 track
 import numpy as np
 import cv2
@@ -14,18 +14,25 @@ from geometry_msgs.msg import Point32
 from std_msgs.msg import *
 from builtin_interfaces.msg import *
 
+np.set_printoptions(threshold=np.inf)
+
+
 class Track(Node):
 
     background = np.zeros(512)
     frame = 0
     threshold = 0.5
+    index_counts = {}
 
     def __init__(self):
         super().__init__('tracking')
         self.bag = self.create_subscription(LaserScan, '/scan', self.bag_callback, 10)
         self.publisher_ = self.create_publisher(PointCloud, '/person_locations', 10)
+        self.people_points = self.create_publisher(PointCloud, '/people_points', 10)
 
     def bag_callback(self, msg):
+
+        print('frame', self.frame)
 
         ranges = np.array(msg.ranges)   # numpy array of lidar ranges
         angles = (np.arange(0, len(msg.ranges)) * msg.angle_increment) + msg.angle_min  # angles corresponding to each range
@@ -35,8 +42,10 @@ class Track(Node):
         y_values = np.sin(angles) * ranges
         z_values = np.zeros(len(angles))
 
+        # push transformed points to publisher_ point cloud (for testing)
         pc = PointCloud()
         pc.points = []
+        # keep put points into an array
         pts = []
 
         for i in range(len(x_values)):
@@ -45,49 +54,64 @@ class Track(Node):
             p.y = y_values[i]
             p.z = z_values[i]
             pc.points.append(p)
+
             pts.append([x_values[i], y_values[i]])
 
         pc.header = msg.header
-
         self.publisher_.publish(pc)
         
-        print("message received")
-        print(z_values)
-        print(x_values)
-        print(y_values)
-        print(pts)
-        exit()
-        return
+        # convert points to matrix
+        pts = np.matrix(pts)
+        # replace inf with 100 so theres no funny business with math
+        pts[pts == np.inf] = 100 
+        pts[pts == -np.inf] = 100
 
-        data = np.array(msg.ranges)
-        data[data == np.inf] = 100
+        if(np.all(pts == 100) or np.all(np.isnan(pts))):
+            print('bad read')
+            return
+
+        # replace nan with neighboring value
+        if(np.any(np.isnan(pts[0]))):
+            pts[0] = pts[1]
+        for i in range(1, len(pts), 1):
+            if np.any(np.isnan(pts[i])):
+                pts[i,:] = pts[i-1,:]
+
 
         # if first frame, save as background
         if self.frame == 0:
-            data[np.isnan(data)] = 100
-            self.background = data
-        else:
-            data[np.isnan(data)] = self.background[np.isnan(data)] # replace faulty points with background
-
-        
-
+            self.background = pts
         
         # backgound is the average of first 5 frames (might need to increase if the data is iffy)
-        if self.frame < 5:
-            self.background = (self.background * self.frame + self.background) / (self.frame + 1)
+        if self.frame < 10:
+            self.background = (self.background * self.frame + pts) / (self.frame + 1)
 
         # subtract background
-        separated = data - self.background
+        separated = pts - self.background
+        separated = np.where(separated < -2, separated, 0) # replace everything over threshold with 0
+        possible_people_points = []
 
-        if(len(separated[abs(separated) > self.threshold])):
-            print('frame', self.frame)
-            print('data\n', data)
-            print('background\n', self.background)
-            print('separated\n', separated)
-            print()
+        # if a row is not 0, add to possible people points
+        for i, row in enumerate(separated):
+            if np.any(row != 0):
+                possible_people_points.append(pts[i])
+                    
+        print(possible_people_points)
+
+        # convert to point could and publish to people_points
+        people_pc = PointCloud()
+        people_pc.points = []
+        for i, pt in enumerate(possible_people_points):
+            p = Point32()
+            p.x = pt[0,0]
+            p.y = pt[0,1]
+            p.z = 0.0
+            people_pc.points.append(p)
+
+        people_pc.header = msg.header
+        self.people_points.publish(people_pc)
 
         self.frame += 1
-
         
 
 def main(args=None):
