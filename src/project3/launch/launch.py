6d487f0077892937d88a24139b0da1c6b6415afc
Carson Duffy <cwduffy01@gmail.com>
2023-03-30 00:06:28 -0500

    doc

--- src/project3/launch/launch.py
+++ src/project3/launch/launch.py
@@ -6,15 +6,17 @@ from launch.event_handlers import *
 from launch.events import *
 
 def generate_launch_description():
-    # node = Node(package='turtlesim',
-    #             executable='turtlesim_node')
-
+    # initialize command line argument
     bag_loc_value = LaunchConfiguration('bag_loc')
     bag_loc_arg = DeclareLaunchArgument('bag_loc', default_value="bags/example1/")
 
-    ep = ExecuteProcess(cmd=['ros2', 'bag', 'play', bag_loc_value])
-    track_node = Node(package='project3', executable='track')
+    ep = ExecuteProcess(cmd=['ros2', 'bag', 'play', bag_loc_value])     # command for playing bag file (with argument)
+    track_node = Node(package='project3', executable='track')   # node for tracking people
+
+    # handle terminating
     event_handler = OnProcessExit(target_action=ep, on_exit=[EmitEvent(event=Shutdown())])
     terminate_at_end = RegisterEventHandler(event_handler)
+
+    # generate launch description and return
     ld = LaunchDescription([ bag_loc_arg, ep, track_node, terminate_at_end ])
     return ld
